d343cabfb4c1218699a66ef355d7d7abfaad84b3
brenno <brennorebeiro@gmail.com>
2024-02-27 20:59:56 -0300

    Add ShowMenusOptions

--- View/GenericMenuView.cs
+++ View/GenericMenuView.cs
@@ -2,7 +2,11 @@ namespace ScreenSound.View{
     internal class GenericMenuView{
         public static void ShowMenusName(string menusName){
             System.Console.WriteLine("==================================================="); 
-            System.Console.WriteLine($"                    {menusName}");
+            System.Console.WriteLine($"{menusName}");
+            System.Console.WriteLine("===================================================");
+        }
+
+        public static void ShowMenusLineSeparation(){
             System.Console.WriteLine("===================================================");
         }
     }
--- View/MenuView.cs
+++ View/MenuView.cs
@@ -3,8 +3,21 @@ namespace ScreenSound.View{
         
         private string menusName => @"𝕊𝕔𝕣𝕖𝕖𝕟𝕊𝕠𝕦𝕟𝕕 𝕄𝕖𝕟𝕦";
 
+        private void ShowAllTheMenuOptions(){
+            System.Console.WriteLine("[1] Add a new Band");
+            System.Console.WriteLine("[2] Add a new Album");
+            System.Console.WriteLine("[3] Add a new Song");
+            System.Console.WriteLine("[4] See the Bands");
+            System.Console.WriteLine("[5] See the Albums");
+            System.Console.WriteLine("[6] See the Songs");
+        }
+
         public void Main(){
             GenericMenuView.ShowMenusName(menusName);
+
+            this.ShowAllTheMenuOptions();
+
+            GenericMenuView.ShowMenusLineSeparation();
         }
     }    
 }
