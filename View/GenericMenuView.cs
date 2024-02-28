2c8034f95db75f57c45145b963d5f4d41dc62b38
brenno <brennorebeiro@gmail.com>
2024-02-27 21:13:48 -0300

    Change Visibility

--- View/GenericMenuView.cs
+++ View/GenericMenuView.cs
@@ -1,13 +1,20 @@
 namespace ScreenSound.View{
     internal class GenericMenuView{
-        public static void ShowMenusName(string menusName){
+
+        protected string? GenericMenusName;
+
+        public GenericMenuView(string menusName){
+            this.GenericMenusName = menusName;
+        }
+        protected void ShowMenusName(){
             System.Console.WriteLine("==================================================="); 
-            System.Console.WriteLine($"{menusName}");
+            System.Console.WriteLine($"{this.GenericMenusName}");
             System.Console.WriteLine("===================================================");
         }
 
-        public static void ShowMenusLineSeparation(){
+        protected void ShowMenusLineSeparation(){
             System.Console.WriteLine("===================================================");
         }
+
     }
 }
