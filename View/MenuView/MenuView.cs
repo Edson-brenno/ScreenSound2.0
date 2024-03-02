f5a5f64743473ad5792d8fad1a8e37202103a1bf
brenno <brennorebeiro@gmail.com>
2024-03-02 11:10:41 -0300

    Change menu nomenclature

--- View/MenuView/MenuView.cs
+++ View/MenuView/MenuView.cs
@@ -50,9 +50,9 @@ namespace ScreenSound.View{
 
         
         private void ShowAllTheMenuOptions(){
-            System.Console.WriteLine("[1] Add a new Band");
-            System.Console.WriteLine("[2] Add a new Album");
-            System.Console.WriteLine("[3] Add a new Song");
+            System.Console.WriteLine("[1] Register a new Band");
+            System.Console.WriteLine("[2] Register a new Album");
+            System.Console.WriteLine("[3] Register a new Song");
             System.Console.WriteLine("[4] See the Bands");
             System.Console.WriteLine("[5] See the Albums");
             System.Console.WriteLine("[6] See the Songs");
