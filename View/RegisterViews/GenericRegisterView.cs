f0e9799352d176cc5f06a33ab5bd9e734d4d48cc
brenno <brennorebeiro@gmail.com>
2024-03-03 10:35:12 -0300

    Add constructor

--- View/RegisterViews/GenericRegisterView.cs
+++ View/RegisterViews/GenericRegisterView.cs
@@ -1,5 +1,11 @@
 namespace ScreenSound.View.RegisterViews{
     internal class GenericRegisterView{
+        protected string? LinesSeparation => "==================================================="; // Separation
+        protected string? registerName; // have the name of the register
+
+        public GenericRegisterView(string titleMenuName){
+            this.registerName = titleMenuName;
+        }
         
     }
 }
